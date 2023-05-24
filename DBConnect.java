ad9c30e8afd5d7de257014daaba6fcb362bd963a
Rondus Technology <rondusllc@gmail.com>
2023-05-23 18:18:30 -0700

    updated DBConnect.java file

--- DBConnect.java
+++
@@ -1,9 +0,0 @@
-Learning Git
-Steps in learning Git
-1. Version control system
-2. Local version control system
-3. central version control system
-4. distributed version control system
-
-And other version control systems
-Yes, Git is different from any other VCS
